2adb6dede074559540a7ba30759b198f2792f0c1
root <root@bogon.(none)>
2019-01-03 15:24:47 +0800

    remove a ignore file

---
+++ src/MySocket_client.h
@@ -0,0 +1,76 @@
+/*
+ * mysocket_client.h
+ *
+ *  Created on: Jun 7, 2017
+ *      Author: gyd
+ */
+#pragma once
+#ifndef MYSOCKET_CLIENT_H_
+#define MYSOCKET_CLIENT_H_
+
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>      // for close , usleep
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>   //inet_pton
+#include <fcntl.h>       // fcntl
+#include <queue>
+#include "Mylog.h"
+
+#define MAXLINE 4096
+#define MAXLENGTH 1024*64
+#ifndef BYTE
+#define BYTE unsigned char
+#endif
+struct MSGBODY
+{
+    BYTE msg[MAXLENGTH];
+    int length;
+};
+/*
+ * use to form a string clientIP:clientPort--> serverIP:serverPort
+ */
+struct CONNECTION
+{
+    int  socket_fd ;       //
+    int  clientPort ;      //
+    int  serverPort ;
+    char clientIP[64] ;
+    char serverIP[64] ;
+};
+class MySocket_client
+{
+public:
+    int mn_socket_fd;
+    MySocket_client();
+    ~MySocket_client();
+    int init(queue<MSGBODY> * msgQToRecv, queue<MSGBODY> * msgQToSend);
+
+    int connectTo(const char* server_IP, int server_port);// connect
+    int setMsg(const char *);
+    int getMsg();
+    int sendMsg();                 // transfer message
+    int fileSend();                // transfer file
+
+    Mylog mylog;
+private:
+
+    struct sockaddr_in m_clientAddr;
+    CONNECTION m_conn;
+
+    queue<MSGBODY>  m_msgQueueRecv;  // a queue to storage the msg
+    queue<MSGBODY>  m_msgQueueSend;
+
+    queue<MSGBODY> * mp_msgQueueRecv; // pointer to queue
+    queue<MSGBODY> * mp_msgQueueSend;
+
+    int myclose();                  // close socket
+};
+
+#endif /* MYSOCKET_CLIENT_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 23, 2017
+ *      Author: gyd
+ */
+
+
+#include <iostream>
+using namespace std;
+#include "MySocket_client.h"
+
+extern int fileTransfer(int argc, char **argv);
+extern int socket_client(char* server_IP);
+int main(int argc, char **argv)
+{
+    MySocket_client sc;
+    int ret = 0;
+
+    queue<MSGBODY> msgQueueRecv;
+    queue<MSGBODY> msgQueueSend;
+    sc.init(&msgQueueRecv, &msgQueueSend);
+    sc.mylog.logException("****************************BEGIN****************************");
+    while (-1 == (ret = sc.connectTo("172.18.10.129", 3401)))
+    {
+        sleep(60);
+    }
+    while(true)
+    {
+        ret = sc.getMsg();
+        if(-1 == ret)       // error
+            continue;
+//		sc.setMsg("\n");
+        ret = sc.sendMsg();
+        if(ret == -1)
+            break;
+    }
+//	fileTransfer(argc, argv);
+
+    return 0;
+}
+
+
